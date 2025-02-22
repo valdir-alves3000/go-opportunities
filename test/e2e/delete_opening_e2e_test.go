47004a7aace760953572454d61a39fedbe63e9f4
valdiralves3000 <valdiralves3000@gmail.com>
2025-02-22 14:40:05 +0000

    test(e2e): add end-to-end tests for opening deletion functionality

---
+++ test/e2e/delete_opening_e2e_test.go
@@ -0,0 +1,111 @@
+package e2e
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/valdir-alves3000/go-opportunities/internal/core/schemas"
+)
+
+func deleteOpening(id uint) *httptest.ResponseRecorder {
+	req, _ := http.NewRequest("DELETE", fmt.Sprintf(basePath+"/openings/%d", id), nil)
+	req.Header.Set("Content-Type", "application/json")
+
+	w := httptest.NewRecorder()
+	router.ServeHTTP(w, req)
+
+	return w
+}
+
+func TestDeleteOpeningE2E(t *testing.T) {
+	clearDatabase := func() {
+		db.Exec("DELETE FROM openings")
+	}
+	t.Run("ShouldDeleteAnOpeningSuccessfully", func(t *testing.T) {
+		clearDatabase()
+		openingReq := schemas.CreateOpeningRequest{
+			Role:     "Go Developer",
+			Company:  "Tech Corp",
+			Location: "Silicon Valley",
+			Link:     "http://example.com",
+			Remote:   new(bool),
+			Salary:   50000,
+		}
+
+		w := createOpening(openingReq)
+		var resp struct {
+			Message string `json:"message"`
+		}
+
+		err := json.Unmarshal(w.Body.Bytes(), &resp)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, http.StatusCreated, w.Code)
+		assert.NoError(t, err)
+		assert.Equal(t, fmt.Sprintf("opening %s created successfully", openingReq.Role), resp.Message)
+
+		var opening schemas.Opening
+		result := db.First(&opening)
+		assert.NoError(t, result.Error)
+		assert.NotZero(t, opening.ID)
+
+		w = deleteOpening(opening.ID)
+
+		err = json.Unmarshal(w.Body.Bytes(), &resp)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, http.StatusOK, w.Code)
+		assert.NoError(t, err)
+		assert.Equal(t, fmt.Sprintf("opening with id: %d deleted successfully", opening.ID), resp.Message)
+
+		result = db.First(&schemas.Opening{}, opening.ID)
+		assert.Error(t, result.Error)
+		assert.Equal(t, "record not found", result.Error.Error())
+	})
+
+	t.Run("ShouldReturnAnErrorIfTheOpeningDoesNotExist", func(t *testing.T) {
+		clearDatabase()
+		w := deleteOpening(1)
+		var resp struct {
+			Message   string `json:"message"`
+			ErrorCode int    `json:"errorCode"`
+		}
+
+		err := json.Unmarshal(w.Body.Bytes(), &resp)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, http.StatusNotFound, w.Code)
+		assert.Equal(t, "opening not found", resp.Message)
+		assert.Equal(t, http.StatusNotFound, resp.ErrorCode)
+	})
+
+	t.Run("ShouldReturnErrorWhenTryingToDeleteOpeningWithInvalidID", func(t *testing.T) {
+		clearDatabase()
+		req, _ := http.NewRequest("DELETE", basePath+"/openings/invalid", nil)
+		req.Header.Set("Content-Type", "application/json")
+
+		w := httptest.NewRecorder()
+		router.ServeHTTP(w, req)
+
+		var resp struct {
+			Message   string `json:"message"`
+			ErrorCode int    `json:"errorCode"`
+		}
+		err := json.Unmarshal(w.Body.Bytes(), &resp)
+
+		assert.Equal(t, http.StatusBadRequest, w.Code)
+		assert.Nil(t, err)
+		assert.Equal(t, "invalid ID", resp.Message)
+		assert.Equal(t, http.StatusBadRequest, resp.ErrorCode)
+	})
+}
